723ffe2a17fadb0e71dfd1378e5eb53dcc1f94ff
Dariusz <dariusz.bakowski0718@gmail.com>
2020-05-11 22:12:40 +0200

    codecov

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using new, Test
 
+
 @testset "minfunc" begin
     @test minfunc(x -> x^3 - x^2 - 1, 1, 2, 20) == 1.4655712316008145
     @test minfunc(x -> x^4 - x^3 - x^2 - 1, 1, 2, 20) == 1.7548776662458072
